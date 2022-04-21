518f1b39858f7122b51150f4b15d077d29f7f75c
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-04-21 14:44:00 -0300

    feat: add CategoriesRepository class & ICreateCategoryDTO interface

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,29 +1,17 @@
 import { Router } from 'express';
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoryRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
 
-  const category = new Category();
-
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
+  categoryRepository.create({ name, description });
 
   return res.status(201).json({
     status: 'success',
     message: 'category created',
-    data: {
-      category,
-    },
   });
 });
 
