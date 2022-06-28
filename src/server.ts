8d8541584baf21022cef1aef7a0cdcc322898495
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-06-28 03:30:49 -0300

    chore: set up typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import { app } from './app';
+import { appDataSource } from './database/app-data-source';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './.env' });
+
+appDataSource
+  .initialize()
+  .then(() => console.log('Data Source has been initialized!'))
+  .catch((err) => console.log('Error during Data Source initialization:', err));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
